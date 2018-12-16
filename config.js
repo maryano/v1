7534c9714e23b20dc0c304c1375c9cfddec2a77e
Mariano Pop <maryanodorin@gmail.com>
2018-12-16 02:53:07 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'riegos-ruiz',
+		wp_ws_url : 'https://ruiz.publiexperiencias.com/wp-appkit-api/riegos-ruiz',
+		wp_url : 'https://ruiz.publiexperiencias.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'Riegos Ruiz',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : ')Rv NjP4sPA/$7=.qJ,>-<$5uF_W/H~ %`NK{ofpx@S{E7tfS:$u6{i+aSyzkH]b',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
